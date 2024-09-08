ec00d6e13c13b12dbf1171b3553f3540e455bd23
TylerMappes <tylermappes@gmail.com>
2024-09-08 10:53:10 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-// src/components/Navbar.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
@@ -8,10 +8,11 @@ const Navbar = () => {
     <nav className="navbar">
       <h1 className="navbar-logo">StreamList</h1>
       <ul className="nav-links">
-        <li><Link to="/">StreamList</Link></li>
-        <li><Link to="/movies">Movies</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li><Link to="/"><i className="fas fa-home"></i> Home</Link></li>
+        <li><Link to="/movies"><i className="fas fa-film"></i> Movies</Link></li>
+        <li><Link to="/cart"><i className="fas fa-shopping-cart"></i> Cart</Link></li>
+        <li><Link to="/events"><i className="fas fa-calendar-plus"></i> Events</Link></li>
+        <li><Link to="/about"><i className="fas fa-info-circle"></i> About</Link></li>
       </ul>
     </nav>
   );
