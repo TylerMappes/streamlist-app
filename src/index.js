8d8794b76de09d0e6ad2b88c6a8a72f5a6a8f71c
TylerMappes <tylermappes@gmail.com>
2024-09-08 10:55:26 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-// src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // Global CSS
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Font Awesome library
 import App from './App'; // Main App component
 
 // Create the root and render the main App component
@@ -11,5 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
