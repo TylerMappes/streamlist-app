edddf96557685cf579940108283b46d4c2198b43
TylerMappes <tylermappes@gmail.com>
2024-09-14 22:26:29 -0500

    Update Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,13 +1,13 @@
-// src/components/Movies.js
 import React from 'react';
 
+// Functional component for the "Movies" page
 const Movies = () => {
   return (
     <div className="movies-container">
       <h2>Movies Page</h2>
-      <p>This page will be developed in Week 4.</p>
+      <p>This page will be developed in Week 4.</p> {/* Placeholder text for future development */}
     </div>
   );
 };
 
-export default Movies;
+export default Movies; // Export component to be used in the app
