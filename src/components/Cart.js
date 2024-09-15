53c24267c63c4ba64f64fc60c14ddd330adf34dd
TylerMappes <tylermappes@gmail.com>
2024-09-14 22:25:22 -0500

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,13 @@
-// src/components/Cart.js
 import React from 'react';
 
+// Functional component for the "Cart" page
 const Cart = () => {
   return (
     <div className="cart-container">
       <h2>Cart Page</h2>
-      <p>This page will be developed in Week 4.</p>
+      <p>This page will be developed in Week 4.</p> {/* Placeholder text for future development */}
     </div>
   );
 };
 
-export default Cart;
+export default Cart; // Export component to be used in the app
