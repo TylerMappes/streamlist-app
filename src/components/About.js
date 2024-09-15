402d828ffc362b20fd7cc0d7a58b0a62c49f6579
TylerMappes <tylermappes@gmail.com>
2024-09-14 22:24:58 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,13 @@
-// src/components/About.js
 import React from 'react';
 
+// Simple functional component for the "About" page
 const About = () => {
   return (
     <div className="about-container">
       <h2>About Page</h2>
-      <p>This page will be developed in Week 5.</p>
+      <p>This page will be developed in Week 5.</p> {/* Placeholder text for future development */}
     </div>
   );
 };
 
-export default About;
+export default About; // Export component to be used in the app
